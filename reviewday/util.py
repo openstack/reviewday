9bbbafddb9d68c27465c4f251e7ae523e33c0d12
Elizabeth Krumbach <lyz@princessleia.com>
2013-01-30 14:27:45 -0800

    Add command line option for output directory.
    Add a -o, --out-dir option to specify an output directory.
    
    Change-Id: I0c8ac1aba309f9a34b2562b18da644efd9f37f4e
    Reviewed-on: https://review.openstack.org/20839
    Reviewed-by: Jeremy Stanley <fungi@yuggoth.org>
    Approved: Dan Prince <dprince@redhat.com>
    Reviewed-by: Dan Prince <dprince@redhat.com>
    Tested-by: Jenkins

--- reviewday/util.py
+++ reviewday/util.py
@@ -4,19 +4,18 @@ from Cheetah.Template import Template
 from distutils.dir_util import copy_tree
 
 
-def prep_out_dir(out_dir='out_report'):
+def prep_out_dir(out_dir):
     src_dir = os.path.dirname(__file__)
     report_files_dir = os.path.join(src_dir, 'report_files')
     copy_tree(report_files_dir, out_dir)
 
 
-def create_report(name_space={}):
+def create_report(out_dir, name_space={}):
     filename = os.path.join(os.path.dirname(__file__), 'report.html')
     report_text = open(filename).read()
     name_space['helper'] = html_helper
     t = Template(report_text, searchList=[name_space])
 
-    out_dir = 'out_report'
     prep_out_dir(out_dir)
 
     out_file = open(os.path.join(out_dir, 'index.html'), "w")
